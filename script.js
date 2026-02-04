d19b5bd850693053ef3392b324466413bbf70a2a
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-02-04 22:10:04 +0500

    4-Updation

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function toggle(){
     var ham = document.querySelector('.sidebar-container');
-    ham.classList.toggle('activeHam')
+    ham.classList.toggle('activeHam');
+}
+
+function desktop_toggle(){
+    var ham = document.querySelector('.sidebar-container');
+    ham.classList.toggle('desktop_Ham');
 }
